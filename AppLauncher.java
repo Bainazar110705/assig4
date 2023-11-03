75bae16d294fddaa41617575ddf566d7d87b232b
Bainazar110705 <149820878+Bainazar110705@users.noreply.github.com>
2023-11-03 23:43:46 +0600

    Create AppLauncher.java

---
+++ AppLauncher.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.example.WeatherAppGui;
+
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                new WeatherAppGui().setVisible(true);
+
+            }
+        });
+    }
+}
